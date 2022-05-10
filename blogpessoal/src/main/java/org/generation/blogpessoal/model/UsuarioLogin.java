3d04733d64568a5b4344bac5679411f647478832
Isabelly Lemos <102272830+isabellylemos@users.noreply.github.com>
2022-05-10 00:16:35 -0300

    Add files via upload

---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/UsuarioLogin.java
@@ -0,0 +1,69 @@
+package org.generation.blogpessoal.model;
+
+
+public class UsuarioLogin {
+	
+	private long Id;
+
+	private String nome;
+
+	private String usuario ;
+
+	private String senha; 
+
+	private String   foto ;
+
+	private String token ;
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	
+	
+	
+}
